4deff4314e365650006c85f7a0373569df5fa25e
Waldemar Renato Batista <34617056+walderdob@users.noreply.github.com>
2026-07-28 13:43:11 -0500

    Add files via upload

---
+++ examples/random_number.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.insert(0, '../')
+from sqp import *
+import torch
+
+def random_number_gen(n):
+    # ψ = |0>^n
+    ψ = kronPower(z_state, n)
+    # Apply Hadamard to all qubits in the state
+    for i in range(n): ψ = qubitWiseMultiply(ψ, n, h_gate, i)
+    
+    # Return the measurement of all qubits
+    return measureState(ψ)
+
+def main():
+    print(random_number_gen(3))
+    
+    
+if __name__ == '__main__':
+    main()
